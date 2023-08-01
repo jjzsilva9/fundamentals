26c2cc810fb12b0449687476eac2443c054a6550
jjzsilva9 <jjzsilva9@gmail.com>
2023-08-01 15:51:12 +0100

    more changes - visual studio code not pulling its weight

--- C++/BinaryTree.cpp
+++ C++/BinaryTree.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <ostream>
 
 class Node {
     private:
-        int val = NULL;
+        int val;
+        //std::unique_ptr<Node> left = nullptr;
         Node* left = nullptr;
+        //std::unique_ptr<Node> right = nullptr;
         Node* right = nullptr;
 
     public:
-        Node(int a) : val{a} {}
+        Node(int a) : val{a} {};
+        Node(int a, Node& const l, Node& const r) : val{a}, left{&l}, right{&r} {};
+
+        ostream& operator<<(ostream& os, const Node& n) {
+            return os << "{" << val << ", " << left << ", " << right << std::endl;
+        };
 
 };
 
 int main() {
-    Node n(5);
+    Node r(6);
+    Node l(4);
+    Node n(5, l, r);
     return 0;
 }
